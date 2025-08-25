7542eadbd79690290b5d2634a7c0093b7bec166a
lhndo <andrei@undoz.com>
2025-08-25 00:32:42 +0000

    cleanup

--- src/simple_cli/cli_commands.rs
+++ src/simple_cli/cli_commands.rs
@@ -29,33 +29,34 @@ pub const CMDS: [Command; NUM_COMMANDS] = [
   },
   Command {
     name: "example",
-    desc: "Print Example | <arg(float)> [opt=0(u8)] [on=false(bool)] [path=\"\"(string)]",
+    desc: "Print Example \n <arg(float)> [opt=0(u8)] [on=false(bool)] [path=\"\"(string)]",
     func: example_cmd,
   },
   Command {
     name: "blink",
-    desc: "Blink Onboard Led | [times=10]",
+    desc: "Blink Onboard Led \n [times=10]",
     func: blink_cmd,
   },
   Command {
     name: "read_adc",
-    desc: "Read ADC | [ref_res=10000(ohm)]",
+    desc: "Read ADC \n [ref_res=10000(ohm)]",
     func: read_adc_cmd,
   },
   Command {
     name: "sample_adc",
-    desc: "Samples ADC GPIO 26 | [channel=0(u8)] [ref_res=10000(ohm)]",
+    desc: "Continuous sampling of an ADC channel \n [channel=0(u8)] [ref_res=10000(ohm)] \
+           [interval=200(ms)] \n Interrupt with char \"~\" ",
     func: sample_adc_cmd,
   },
   Command {
     name: "servo",
-    desc: "Set Servo PWM on GPIO 2 | [us=1500(us)] [pause=1500(ms)] [sweep=false(bool)] \
-           [max_us=2000(u16)]",
+    desc: "Set Servo PWM on GPIO 2 \n [us=1500(us)] [pause=1500(ms)] [sweep=false(bool)] \
+           [max_us=2000(us)]",
     func: servo_cmd,
   },
   Command {
     name: "set_pwm",
-    desc: "Sets PWM on GPIO 6 | [slice=3(u8)] [freq=50(hz)] [us=-1(us)] [duty=50(%)] \n \
+    desc: "Sets PWM on GPIO 6 \n [slice=3(u8)] [freq=50(hz)] [us=-1(us)] [duty=50(%)] \n \
            [top=-1(u16)] [phase=false(bool)] [disable=false(bool)]",
     func: set_pwm_cmd,
   },
@@ -73,7 +74,7 @@ type Context = Device;
 fn help_cmd(args: &[Arg], device: &mut Context) -> Result<()> {
   println!("Available commands:\n");
   for cmd in CMDS.iter() {
-    println!(" {} - {} ", &cmd.name, &cmd.desc);
+    println!(" {} - {} \n", &cmd.name, &cmd.desc);
   }
   Ok(())
 }
@@ -176,11 +177,12 @@ fn read_adc(device: &mut Context, ref_res: u32) -> Result<()> {
 fn sample_adc_cmd(args: &[Arg], device: &mut Context) -> Result<()> {
   let ref_res: u32 = get_parsed_param("ref_res", args).unwrap_or(10_000);
   let channel: u8 = get_parsed_param("channel", args).unwrap_or(0);
+  let interval: u16 = get_parsed_param("interval", args).unwrap_or(200);
 
-  sample_adc(device, channel, ref_res)
+  sample_adc(device, channel, ref_res, interval)
 }
 
-fn sample_adc(device: &mut Context, channel: u8, ref_res: u32) -> Result<()> {
+fn sample_adc(device: &mut Context, channel: u8, ref_res: u32, interval: u16) -> Result<()> {
   println!("---- Sample ADC ----");
   println!("Reference Pullup Resistor: {}ohm", ref_res);
   println!("ADC Channel: {} \n", { channel });
@@ -191,7 +193,7 @@ fn sample_adc(device: &mut Context, channel: u8, ref_res: u32) -> Result<()> {
       let adc_vol = adc_raw.to_voltage();
       let adc_res = adc_raw.to_resistance(ref_res);
       println!("> v:{:.2}, ohm:{:.1}, raw:{} \r", adc_vol, adc_res, adc_raw);
-      device.timer.delay_ms(400);
+      device.timer.delay_ms(interval as u32);
     } else {
       println!("Cannot read channel: {}", channel);
     }
@@ -210,14 +212,14 @@ fn sample_adc(device: &mut Context, channel: u8, ref_res: u32) -> Result<()> {
 
 fn servo_cmd(args: &[Arg], device: &mut Context) -> Result<()> {
   let us: u16 = get_parsed_param("us", args).unwrap_or(1500); //  1500 us default
-  let pause: u16 = get_parsed_param("pause", args).unwrap_or(1500); // 2s default
+  let pause: u32 = get_parsed_param("pause", args).unwrap_or(1500); // 2s default
   let sweep: bool = get_parsed_param("sweep", args).unwrap_or(false); //  false default
   let max_us: u16 = get_parsed_param("max_us", args).unwrap_or(2000); //  2000 us default
 
   servo(device, us, pause, sweep, max_us)
 }
 
-fn servo(device: &mut Context, us: u16, pause: u16, sweep: bool, max_us: u16) -> Result<()> {
+fn servo(device: &mut Context, us: u16, pause: u32, sweep: bool, max_us: u16) -> Result<()> {
   println!("---- Servo ----");
   println!("GPIO 8 pwm4A");
 
@@ -235,26 +237,26 @@ fn servo(device: &mut Context, us: u16, pause: u16, sweep: bool, max_us: u16) ->
   pwm_pin.set_duty_cycle_us(us, FREQ);
   pwm_slice.enable();
   println!("Moving...");
-  device.timer.delay_ms(pause as u32);
+  device.timer.delay_ms(pause);
 
   if sweep {
     let pwm_pin = pwm_slice.get_channel_a();
     println!("Sweeping...");
     //Max
     pwm_pin.set_duty_cycle_us(max_us, FREQ);
-    device.timer.delay_ms(pause as u32);
+    device.timer.delay_ms(pause);
 
     //Mid
     pwm_pin.set_duty_cycle_us(MID, FREQ);
-    device.timer.delay_ms(pause as u32);
+    device.timer.delay_ms(pause);
 
     //Min
     pwm_pin.set_duty_cycle_us(min_us, FREQ);
-    device.timer.delay_ms(pause as u32);
+    device.timer.delay_ms(pause);
 
     //Mid
     pwm_pin.set_duty_cycle_us(MID, FREQ);
-    device.timer.delay_ms(pause as u32);
+    device.timer.delay_ms(pause);
   }
 
   //Off
